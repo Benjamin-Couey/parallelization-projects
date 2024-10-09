4b476001371d3d792ed722427da83352e8e86abc
Benjamin-Couey <benjamincouey36@gmail.com>
2024-10-09 08:05:07 -0600

    Updated par_main.c to calculate whether numbers are prime in batches to improve performance.

--- par_main.c
+++ par_main.c
@@ -15,21 +15,24 @@ int main(int argc, char **argv){
   MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
   MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
 
-	int n;
+	int n, batchSize;
 
-	if (argc != 2) {
-    fprintf(stderr,"Please provide the following on the command line: n.\n");
+	if (argc != 3) {
+    fprintf(stderr,"Please provide the following on the command line: n and batchSize.\n");
 		MPI_Finalize();
     return 0;
   }
 
 	sscanf(argv[1],"%d",&n);
+	sscanf(argv[2],"%d",&batchSize);
 
 	double begin = MPI_Wtime();
 
 	// Variables all processes will use.
 	int foundNthPrime = 0;
 	int iteration = 0;
+	int primesPerIter = nprocs * batchSize;
+	int * resultBatch = (int*)malloc(sizeof(int)*batchSize);
 
 	// Variables only the root process will use.
 	int numTwins;
@@ -38,7 +41,8 @@ int main(int argc, char **argv){
 	int * foundNthPrimeBuffer;
 	if(my_rank == ROOT_RANK){
 		numTwins = 0;
-		gatheredResults = (int*)malloc(sizeof(int)*nprocs);
+		lastPrime = 2;
+		gatheredResults = (int*)malloc(sizeof(int)*primesPerIter);
 		foundNthPrimeBuffer = (int*)malloc(sizeof(int)*nprocs);
 		for( int index=0; index<nprocs; index++ ){
 			foundNthPrimeBuffer[index] = 0;
@@ -47,18 +51,18 @@ int main(int argc, char **argv){
 
 	while( !foundNthPrime ){
 		// Each process calculates if a different number is prime.
-		int num = 2 + my_rank + (nprocs * iteration);
-		int result = IsPrime( num );
+		for( int index=0; index<batchSize; index++ ){
+			int num = 2 + my_rank * batchSize + (primesPerIter * iteration) + index;
+			resultBatch[ index ] = IsPrime( num );
+		}
 
-		// TODO: Avoid synchronizing every iteration, as this will massively hurt
-		// performance.
 		// Gather results of prime calculations.
-		MPI_Gather( &result, 1, MPI_INT, gatheredResults, 1, MPI_INT, ROOT_RANK, MPI_COMM_WORLD );
+		MPI_Gather( resultBatch, batchSize, MPI_INT, gatheredResults, batchSize, MPI_INT, ROOT_RANK, MPI_COMM_WORLD );
 
 		// Have the root process check for the presence of twin primes.
 		if(my_rank == ROOT_RANK){
-			for( int index=0; index<nprocs; index++ ){
-				int num = 2 + index + (nprocs * iteration);
+			for( int index=0; index<primesPerIter; index++ ){
+				int num = 2 + index + (primesPerIter * iteration);
 				if( gatheredResults[index] ){
 					if( num - 2 == lastPrime ){
 						numTwins++;
@@ -88,6 +92,7 @@ int main(int argc, char **argv){
 	  printf("Took %f seconds.\n", seconds);
 	}
 
+	free(resultBatch);
 	if(my_rank == ROOT_RANK) {
     free(gatheredResults);
 		free(foundNthPrimeBuffer);
