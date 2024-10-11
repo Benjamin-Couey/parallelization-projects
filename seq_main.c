ba154f1590614287f2f91f29254917afe2e7b7e2
Benjamin-Couey <benjamincouey36@gmail.com>
2024-10-11 10:24:37 -0600

    Updated seq_main.c to have more sophisticated arg parsing with argp.h

--- seq_main.c
+++ seq_main.c
@@ -1,19 +1,59 @@
+#include <argp.h>
 #include <stdio.h>
 #include <time.h>
 
 #include "seq_twin_prime.c"
 
+static char doc[] = "seq_twin_prime -- A simple sequential C script that calculates the nth twin prime.";
+
+static char args_doc[] = "Number of twin primes to calculate.";
+
+static struct argp_option options[] = {
+	{ "verbose", 'v', 0, 0, "Provide verbose output." },
+	{ 0 }
+};
+
+struct arguments {
+	char *args[1];
+	int verbose;
+};
+
+static error_t ParseOpt( int key, char *arg, struct argp_state *state) {
+	struct arguments *arguments = state->input;
+	switch(key) {
+		case 'v':
+			arguments->verbose = 1;
+			break;
+		case ARGP_KEY_ARG:
+			if( state->arg_num >= 1 ){
+				argp_usage( state );
+			}
+			arguments->args[state->arg_num] = arg;
+			break;
+		case ARGP_KEY_END:
+			if( state->arg_num < 1 ){
+				argp_usage( state );
+			}
+			break;
+		default:
+			return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
+}
+
+static struct argp argp = { options, ParseOpt, args_doc, doc };
+
 int main(int argc, char **argv){
 
 	int n, verbose;
 
-	if (argc != 3) {
-    fprintf(stderr,"Please provide the following on the command line: n and verbose.\n");
-    return 0;
-  }
+	struct arguments arguments;
+	arguments.verbose = 0;
 
-	sscanf(argv[1],"%d",&n);
-	sscanf(argv[2],"%d",&verbose);
+	argp_parse (&argp, argc, argv, 0, 0, &arguments);
+
+	sscanf(arguments.args[0],"%d",&n);
+	verbose = arguments.verbose;
 
 	clock_t begin, end;
 	if( verbose ){
