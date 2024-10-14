1124b560ac6c6bfbf613b94b2f180f222a518166
Benjamin-Couey <benjamincouey36@gmail.com>
2024-10-14 08:53:07 -0600

    Updated par_main.c to have more sophisticated arg parsing with argp.h

--- par_main.c
+++ par_main.c
@@ -1,3 +1,4 @@
+#include <argp.h>
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,7 +8,49 @@
 
 #define ROOT_RANK 0
 
+static char doc[] = "par_twin_prime -- A simple C script, parallelized with MPI, that calculates the nth twin prime. Should be executed with mpirun.";
+
+static char args_doc[] = "Number of twin primes to calculate Size of batch";
+
+static struct argp_option options[] = {
+	{ "verbose", 'v', 0, 0, "Provide verbose output." },
+	{ 0 }
+};
+
+struct arguments {
+	char *args[2];
+	int verbose;
+};
+
+static error_t ParseOpt( int key, char *arg, struct argp_state *state) {
+	struct arguments *arguments = state->input;
+	switch(key) {
+		case 'v':
+			arguments->verbose = 1;
+			break;
+		case ARGP_KEY_ARG:
+			if( state->arg_num >= 2 ){
+				argp_usage( state );
+			}
+			arguments->args[state->arg_num] = arg;
+			break;
+		case ARGP_KEY_END:
+			if( state->arg_num < 2 ){
+				argp_usage( state );
+			}
+			break;
+		default:
+			return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
+}
+
+static struct argp argp = { options, ParseOpt, args_doc, doc };
+
 int main(int argc, char **argv){
+
+	int n, batchSize, verbose;
+	int * argumentsBuffer = (int*)malloc(sizeof(int)*3);
 
 	int my_rank, nprocs;
 
@@ -15,18 +58,28 @@ int main(int argc, char **argv){
   MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
   MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
 
-	int n, batchSize;
+	// Have the root process parse the command line arguments so any output will
+	// only be printed once.
+	if(my_rank == ROOT_RANK) {
+		struct arguments arguments;
+		arguments.verbose = 0;
 
-	if (argc != 3) {
-    fprintf(stderr,"Please provide the following on the command line: n and batchSize.\n");
-		MPI_Finalize();
-    return 0;
-  }
+		argp_parse (&argp, argc, argv, 0, 0, &arguments);
 
-	sscanf(argv[1],"%d",&n);
-	sscanf(argv[2],"%d",&batchSize);
+		sscanf(arguments.args[0],"%d",&argumentsBuffer[0]);
+		sscanf(arguments.args[1],"%d",&argumentsBuffer[1]);
+		argumentsBuffer[2] = arguments.verbose;
+	}
 
-	double begin = MPI_Wtime();
+	// Broadcast the command line arguments processed by root.
+	// TODO: Calculate number of values to send from structure of arguments
+	MPI_Bcast( argumentsBuffer, 3, MPI_INT, ROOT_RANK, MPI_COMM_WORLD );
+
+	n = argumentsBuffer[0];
+	batchSize = argumentsBuffer[1];
+	verbose = argumentsBuffer[2];
+
+	free(argumentsBuffer);
 
 	// Variables all processes will use.
 	int foundNthPrime = 0;
@@ -39,6 +92,7 @@ int main(int argc, char **argv){
 	long long lastPrime;
 	int * gatheredResults;
 	int * foundNthPrimeBuffer;
+	double begin;
 	if(my_rank == ROOT_RANK){
 		numTwins = 0;
 		lastPrime = 2;
@@ -46,6 +100,9 @@ int main(int argc, char **argv){
 		foundNthPrimeBuffer = (int*)malloc(sizeof(int)*nprocs);
 		for( int index=0; index<nprocs; index++ ){
 			foundNthPrimeBuffer[index] = 0;
+		}
+		if( verbose ){
+			begin = MPI_Wtime();
 		}
 	}
 
@@ -85,10 +142,9 @@ int main(int argc, char **argv){
 		iteration++;
 	}
 
-	double end = MPI_Wtime();
-	double seconds = end - begin;
-
-	if(my_rank == ROOT_RANK){
+	if(my_rank == ROOT_RANK && verbose){
+		double end = MPI_Wtime();
+		double seconds = end - begin;
 	  printf("Took %f seconds.\n", seconds);
 	}
 
