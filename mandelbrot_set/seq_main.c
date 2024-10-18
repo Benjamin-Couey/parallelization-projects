23e918ab5f8ac396359a5a73f5856f749ba3b22c
Benjamin-Couey <benjamincouey36@gmail.com>
2024-10-18 12:36:04 -0600

    Updated main script to calculate whether points in range are in Mandelbrot set and write result to a .csv file

--- mandelbrot_set/seq_main.c
+++ mandelbrot_set/seq_main.c
@@ -6,12 +6,26 @@
 int main(int argc, char **argv){
 
 	int max_iterations = 100;
+	int x_resolution = 144;
+	int y_resolution = 144;
 
-	double complex c = -1 + 0 * I;
+	double x_step = 4.0 / x_resolution;
+	double y_step = 4.0 / x_resolution;
 
-	int iterations = in_mandelbrot_set( c, max_iterations );
-
-	printf("in_mandelbrot_set exited after %d iterations.\n", iterations);
+	FILE * file;
+	file = fopen("mandelbrot_set.csv", "w+");
+	for( double y=-2.0; y<4.0; y+=y_step ){
+		for( double x=-2.0; x<4.0; x+=x_step ){
+			double complex c = x + y * I;
+			int iterations = in_mandelbrot_set( c, max_iterations );
+			fprintf(file, "%d", iterations);
+			if( x + x_step < 4.0 ){
+				fprintf(file, ",");
+			}
+		}
+		fprintf(file, "\n");
+	}
+	fclose(file);
 
 	return 0;
 }
