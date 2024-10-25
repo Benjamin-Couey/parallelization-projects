27babc0215603f97393633885982756150e47df5
Benjamin-Couey <benjamincouey36@gmail.com>
2024-10-25 09:43:53 -0600

    Updated the way the result buffers are gathered so the order is preserved

--- mandelbrot_set/par_main.c
+++ mandelbrot_set/par_main.c
@@ -101,149 +101,20 @@ int main(int argc, char **argv){
 	// For high limits, the bulk of the computational time will be spent verifying
 	// points that are inside the Mandelbrot set.
 	// Since these points are densest for y values between about -0.66 and 0.66,
-	// in an effort to balance the load that range will be divided evenly between
-	// all the ranks. The remaining y values will be divided seperately.
+	// in an effort to balance the load that range will be divided seperate from the
+	// other y values. This will create 3 chunks of y values that each need to be
+	// divided amongst the ranks.
 
-	// The range of -0.66 to 0.66 is about 0.44 or 4/9ths of the full -1.5 to 1.5
-	// range of y values the script is calculating. This leaves two low density areas,
-	// each about 5/18ths on either side.
+	// The range of -0.66 to 0.66 is about 1.33/3 or 4/9ths of the full -1.5 to 1.5
+	// range of y values the script is calculating. This creates one high density
+	// chunk with a low density chunk, each about 5/18ths, on either side.
 	int low_density_size = y_resolution * 5 / 18;
 	int high_density_size = y_resolution * 4 / 9;
-
-	printf(
-		"low_density_size is %d and high_density_size is %d\n",
-		low_density_size,
-		high_density_size
-	);
-
-	// Variables all processes will use.
-	double x, y;
-	int buffer_i = 0;
-	int y_per_rank;
-	int points_per_rank = 0;
-	// An array to store the starting and max y indices for the low and high density
-	// y values each rank is responsible for.
-	int * y_i = (int*)malloc(sizeof(int)*3);
-	int * max_y_i = (int*)malloc(sizeof(int)*3);
-	// An array to store the size of each rank's result buffer.
-  int * num_to_send = (int*)malloc(sizeof(int)*n_procs);
-	for( int index=0; index<n_procs; index++ ){
-		num_to_send[index] = 0;
-	}
-	// Have each rank calculate the range of y values each rank will be responsible
-	// for and size of each rank's result buffer.
-	// Technically, only the root process needs this information for MPI_Gatherv,
-	// but it is convienent to do all these calculations in one place.
-
- 	// TODO: This is currently bad from a readability/maintainability standpoint.
-	// Consider a simpler way to divy up the y values?
-
-	// Divide the first low density y indices.
-	y_per_rank = low_density_size / n_procs;
-	for( int index=0; index<n_procs; index++ ){
-		int temp_y_i = y_per_rank * index;
-		int temp_max_y_i = y_per_rank * (index + 1);
-		// Assign any leftover y points to the last rank.
-		if( index == n_procs-1 ){
-			temp_max_y_i = low_density_size;
-		}
-		int temp_points_per_rank = (temp_max_y_i-temp_y_i) * x_resolution;
-		num_to_send[index] += temp_points_per_rank;
-		// Save the results of this calculation to the approprite rank.
-		if( my_rank == index ){
-			y_i[0] = temp_y_i;
-			max_y_i[0] = temp_max_y_i;
-			points_per_rank += temp_points_per_rank;
-		}
-	}
-
-	// Divide the high density y indices.
-	y_per_rank = high_density_size / n_procs;
-	for( int index=0; index<n_procs; index++ ){
-		int temp_y_i = y_per_rank * index + low_density_size;
-		int temp_max_y_i = y_per_rank * (index + 1) + low_density_size;
-		// Assign any leftover y points to the last rank.
-		if( index == n_procs-1 ){
-			temp_max_y_i = low_density_size + high_density_size;
-		}
-		int temp_points_per_rank = (temp_max_y_i-temp_y_i) * x_resolution;
-		num_to_send[index] += temp_points_per_rank;
-		// Save the results of this calculation to the approprite rank.
-		if( my_rank == index ){
-			y_i[1] = temp_y_i;
-			max_y_i[1] = temp_max_y_i;
-			points_per_rank += temp_points_per_rank;
-		}
-	}
-
-	// Divide the second low density y indices.
-	y_per_rank = low_density_size / n_procs;
-	for( int index=0; index<n_procs; index++ ){
-		int temp_y_i = y_per_rank * index + low_density_size + high_density_size;
-		int temp_max_y_i = y_per_rank * (index + 1) + low_density_size + high_density_size;
-		// Assign any leftover y points to the last rank.
-		if( index == n_procs-1 ){
-			temp_max_y_i = y_resolution;
-		}
-		int temp_points_per_rank = (temp_max_y_i-temp_y_i) * x_resolution;
-		num_to_send[index] += temp_points_per_rank;
-		// Save the results of this calculation to the approprite rank.
-		if( my_rank == index ){
-			y_i[2] = temp_y_i;
-			max_y_i[2] = temp_max_y_i;
-			points_per_rank += temp_points_per_rank;
-		}
-	}
-
-	// A buffer to store the results of a rank's calculations.
-	struct in_set_result * result_buffer = (struct in_set_result*)malloc(sizeof(struct in_set_result)*points_per_rank);
-
-	// Variables only the root process will use.
-	// A buffer to store the gathered results of all the ranks' calculations.
-	struct in_set_result * gathered_results;
-	// An array to store the displacement of the results the root rank will be
-  // receiving.
-  int * displacement;
-
-	if(my_rank == ROOT_RANK) {
-		gathered_results = (struct in_set_result*)malloc(sizeof(struct in_set_result)*x_resolution*y_resolution);
-		displacement = (int*)malloc(sizeof(int)*n_procs);
-		for ( int index=0; index<n_procs; index++) {
-			displacement[ index ] = (index > 0) ? ( displacement[ index-1 ] + num_to_send[ index-1 ] ) : 0;
-	  }
-	}
-
-	if( verbose ){
-		set_calc_begin = clock();
-	}
-
-	// printf("Rank %d calculating points for y from %d to %d for a total of %d points\n", my_rank, y_i, max_y_i, points_per_rank);
-	// Calculate Mandelbrot set in range, store results in buffer.
-	for( int index=0; index<3; index++ ){
-		int y_index = y_i[index];
-		while( y_index<max_y_i[index] ){
-			y = -1.5 + (y_index * y_step);
-			for( int x_i=0; x_i<x_resolution; x_i++ ){
-				x = -2.0 + x_i * x_step;
-				// printf("Rank %d calculating point %f,%f\n", my_rank, x, y);
-				double complex c = x + y * I;
-				struct in_set_result result;
-				result.x = x;
-				result.y = y;
-				result.iterations = in_mandelbrot_set( c, max_iterations );
-				result_buffer[ buffer_i ] = result;
-				buffer_i++;
-			}
-			y_index++;
-		}
-	}
-
-	if( verbose ){
-		set_calc_end = clock();
-	  double seconds = (double)(set_calc_end - set_calc_begin) / CLOCKS_PER_SEC;
-		printf("Rank %d took %f seconds to calculate its share of the points.\n", my_rank, seconds);
-	}
-
+	int num_chunks = 3;
+	int * chunk_sizes = (int*)malloc(sizeof(int)*num_chunks);
+	chunk_sizes[0] = low_density_size;
+	chunk_sizes[1] = high_density_size;
+	chunk_sizes[2] = low_density_size;
 
 	//Create a datatype for the nth worker_results[n] struct
 	MPI_Datatype mpi_in_set_result;
@@ -263,18 +134,124 @@ int main(int argc, char **argv){
 	MPI_Type_create_struct( 3, lengths, displacements, types, &mpi_in_set_result );
 	MPI_Type_commit(&mpi_in_set_result);
 
+	// An array to store the starting and max y values for the chunks of y values
+	// each ranks is responsible for.
+	int * y_i = (int*)malloc(sizeof(int)*num_chunks);
+	int * max_y_i = (int*)malloc(sizeof(int)*num_chunks);
+	// A 2D array to store the size of each rank's result buffers for each chunk
+	// of y values.
+	int ** num_to_send = (int**)malloc(sizeof(int*)*num_chunks);
+  for (int chunk_i=0; chunk_i<num_chunks; chunk_i++) {
+    num_to_send[chunk_i]=(int*)malloc(sizeof(int)*n_procs);
+  }
+
+	// Have all ranks calculate the range of y values each rank will be responsible
+	// for for each chunk of y values.
+	// Technically, only the root process needs this information from other ranks
+	// for MPI_Gatherv, but it is convienent to do all these calculations in one
+	// place.
+	int y_offset = 0;
+	for( int chunk_i=0; chunk_i<num_chunks; chunk_i++ ){
+		int y_per_rank = chunk_sizes[ chunk_i ] / n_procs;
+		if( chunk_i > 0 ){
+			y_offset += chunk_sizes[ chunk_i-1 ];
+		}
+		for( int rank_i=0; rank_i<n_procs; rank_i++ ){
+			int temp_y_i = y_per_rank * rank_i + y_offset;
+			int temp_max_y_i = y_per_rank * (rank_i + 1) + y_offset;
+			if( rank_i == n_procs-1 ){
+				if( chunk_i == num_chunks-1 ){
+					temp_max_y_i = y_resolution;
+				}else{
+					temp_max_y_i = y_offset + chunk_sizes[ chunk_i ];
+				}
+			}
+			int temp_points_per_rank = (temp_max_y_i-temp_y_i) * x_resolution;
+			num_to_send[chunk_i][rank_i] = temp_points_per_rank;
+			if( my_rank == rank_i ){
+				y_i[chunk_i] = temp_y_i;
+				max_y_i[chunk_i] = temp_max_y_i;
+			}
+		}
+	}
+
+	// The buffers to store the results of a rank's calculations.
+	struct in_set_result ** result_buffer = (struct in_set_result**)malloc(sizeof(struct in_set_result*)*num_chunks);
+  for (int chunk_i=0; chunk_i<num_chunks; chunk_i++) {
+    result_buffer[chunk_i]=(struct in_set_result*)malloc(sizeof(struct in_set_result)*num_to_send[chunk_i][my_rank]);
+  }
+
+	// Variables only the root process will use.
+	// A buffer to store the gathered results of all the ranks' calculations.
+	struct in_set_result * gathered_results;
+	// A 2D array to store the displacement of the results the root rank will be
+  // receiving for each chunk of y values.
+  int ** displacement = (int**)malloc(sizeof(int*)*num_chunks);
+
+	if(my_rank == ROOT_RANK) {
+		gathered_results = (struct in_set_result*)malloc(sizeof(struct in_set_result)*x_resolution*y_resolution);
+	  for (int chunk_i=0; chunk_i<num_chunks; chunk_i++) {
+	    displacement[chunk_i]=(int*)malloc(sizeof(int)*n_procs);
+			for ( int rank_i=0; rank_i<n_procs; rank_i++) {
+				if( rank_i > 0 ){
+					displacement[chunk_i][rank_i] = displacement[chunk_i][rank_i-1] + num_to_send[chunk_i][rank_i-1];
+				} else {
+					if( chunk_i > 0 ) {
+						displacement[chunk_i][rank_i] = displacement[chunk_i-1][n_procs-1] + num_to_send[chunk_i-1][n_procs-1];
+					} else {
+						displacement[chunk_i][rank_i] == 0;
+					}
+				}
+		  }
+	  }
+	}
+
+	if( verbose ){
+		set_calc_begin = clock();
+	}
+
+	// printf("Rank %d calculating points for y from %d to %d for a total of %d points\n", my_rank, y_i, max_y_i, points_per_rank);
+	// Calculate Mandelbrot set in range, store results in buffer.
+	for( int chunk_i=0; chunk_i<num_chunks; chunk_i++ ){
+		int y_index = y_i[chunk_i];
+		int buffer_i = 0;
+		while( y_index<max_y_i[chunk_i] ){
+			double y = -1.5 + (y_index * y_step);
+			for( int x_i=0; x_i<x_resolution; x_i++ ){
+				double x = -2.0 + x_i * x_step;
+				// printf("Rank %d calculating point %f,%f\n", my_rank, x, y);
+				double complex c = x + y * I;
+				struct in_set_result result;
+				result.x = x;
+				result.y = y;
+				result.iterations = in_mandelbrot_set( c, max_iterations );
+				result_buffer[chunk_i][ buffer_i ] = result;
+				buffer_i++;
+			}
+			y_index++;
+		}
+	}
+
+	if( verbose ){
+		set_calc_end = clock();
+	  double seconds = (double)(set_calc_end - set_calc_begin) / CLOCKS_PER_SEC;
+		printf("Rank %d took %f seconds to calculate its share of the points.\n", my_rank, seconds);
+	}
+
 	// Gather results of Mandelbrot set calculations
-	MPI_Gatherv(
-		result_buffer,
-		points_per_rank,
-		mpi_in_set_result,
-		gathered_results,
-		num_to_send,
-		displacement,
-		mpi_in_set_result,
-		ROOT_RANK,
-		MPI_COMM_WORLD
-	);
+	for( int chunk_i=0; chunk_i<num_chunks; chunk_i++){
+		MPI_Gatherv(
+			result_buffer[chunk_i],
+			num_to_send[chunk_i][my_rank],
+			mpi_in_set_result,
+			gathered_results,
+			num_to_send[chunk_i],
+			displacement[chunk_i],
+			mpi_in_set_result,
+			ROOT_RANK,
+			MPI_COMM_WORLD
+		);
+	}
 
 	// if( my_rank == ROOT_RANK ){
 	// 	for( int b_i=0; b_i<(x_resolution*y_resolution); b_i++ ){
@@ -305,14 +282,24 @@ int main(int argc, char **argv){
 		fclose(file);
 	}
 
+	free(chunk_sizes);
 	free(y_i);
 	free(max_y_i);
-	free(num_to_send);
+	for(int chunk_i=0; chunk_i<num_chunks; chunk_i++){
+    free(num_to_send[chunk_i]);
+  }
+  free(num_to_send);
+	for(int chunk_i=0; chunk_i<num_chunks; chunk_i++){
+    free(result_buffer[chunk_i]);
+  }
 	free(result_buffer);
 	if(my_rank == ROOT_RANK) {
-		free(displacement);
+		for(int chunk_i=0; chunk_i<num_chunks; chunk_i++){
+	    free(displacement[chunk_i]);
+	  }
 		free(gathered_results);
 	}
+	free(displacement);
 
 	if( my_rank == ROOT_RANK && verbose ){
 		end = clock();
